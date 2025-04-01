1833185deba55f3bcd3e1439e4e725918c0d5c3b
fxz200 <fxzxz200@gmail.com>
2025-04-01 16:54:22 +0800

    implement CORS middleware to allow requests

--- backend/router/router.go
+++ backend/router/router.go
@@ -12,28 +12,42 @@ import (
 func SetRouter(port int) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	r.Use(func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		if origin == "http://localhost:3001" || origin == "null" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin) // 僅允許 localhost:3001
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Requested-With, Accept, Authorization, Sec-Ch-Ua, Sec-Ch-Ua-Mobile, Sec-Ch-Ua-Platform")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
 	url := ginSwagger.URL(fmt.Sprintf("http://localhost:%d/swagger/doc.json", port))
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler, url))
 	sprint := r.Group("api/v1/sprint")
 	{
-		sprint.GET("/", controller.GetAllSprint)
-		sprint.POST("/", controller.CreateSprint)
-		sprint.PUT("/", controller.UpdateSprint)
-		sprint.DELETE("/", controller.DeleteSprint)
+		sprint.GET("", controller.GetAllSprint)
+		sprint.POST("", controller.CreateSprint)
+		sprint.PUT("", controller.UpdateSprint)
+		sprint.DELETE("", controller.DeleteSprint)
 	}
 	member := r.Group("api/v1/member")
 	{
-		member.GET("/", controller.GetMembers)
-		member.POST("/", controller.CreateMember)
-		member.PUT("/", controller.UpdateMember)
-		member.DELETE("/", controller.DeleteMember)
+		member.GET("", controller.GetMembers)
+		member.POST("", controller.CreateMember)
+		member.PUT("", controller.UpdateMember)
+		member.DELETE("", controller.DeleteMember)
 	}
 	ticket := r.Group("api/v1/ticket")
 	{
-		ticket.GET("/", controller.GetTickets)
-		ticket.POST("/", controller.CreateTicket)
-		ticket.PUT("/", controller.UpdateTicket)
-		ticket.DELETE("/", controller.DeleteTicket)
+		ticket.GET("", controller.GetTickets)
+		ticket.POST("", controller.CreateTicket)
+		ticket.PUT("", controller.UpdateTicket)
+		ticket.DELETE("", controller.DeleteTicket)
 	}
 	return r
 }
