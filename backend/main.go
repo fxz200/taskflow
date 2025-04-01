4b8a77c2bc72aa04e607feb1cf386035e96b97f8
fxz200 <fxzxz200@gmail.com>
2025-04-01 14:02:24 +0800

    restrict CORS to localhost:3001

--- backend/main.go
+++ backend/main.go
@@ -14,29 +14,32 @@ import (
 )
 
 func main() {
-    if err := sql.InitPostgres(); err != nil {
-        panic(err)
-    }
-    port := 8080
-    // sql.Connect.AutoMigrate(&model.Message{}, &model.User{})
-    sql.Connect.AutoMigrate( &model.Sprint{} , &model.Member{})
-    r := router.SetRouter(port)
-    //for froentend to access backend
-    r.Use(func(c *gin.Context) {
-        c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-        c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-        c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
-        c.Next()
-    })
-    r.GET("/api/message", func(c *gin.Context) {
-        c.JSON(http.StatusOK, gin.H{"message": "Hello from Go backend!"})
-    })
-    fmt.Println("backend Server Start")
-    if err := r.Run(":8080"); err != nil {
-        log.Fatalf("backend Server fail: %v", err)
-    }
+	if err := sql.InitPostgres(); err != nil {
+		panic(err)
+	}
+	port := 8080
+	// sql.Connect.AutoMigrate(&model.Message{}, &model.User{})
+	sql.Connect.AutoMigrate(&model.Sprint{}, &model.Member{}, &model.Ticket{})
+	r := router.SetRouter(port)
+	//for froentend to access backend
+	r.Use(func(c *gin.Context) {
+		origin := c.Request.Header.Get("Origin")
+		if origin == "http://localhost:3001" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin) // 僅允許 localhost:3001
+		}
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+	r.GET("/api/message", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"message": "Hello from Go backend!"})
+	})
+	fmt.Println("backend Server Start")
+	if err := r.Run(":8080"); err != nil {
+		log.Fatalf("backend Server fail: %v", err)
+	}
 }
