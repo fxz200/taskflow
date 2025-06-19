f6b8f4d12ecce191fc479b70e039e774bb7c9a40
fxz200 <fxzxz200@gmail.com>
2025-06-19 11:04:24 +0800

    relase date not required

--- backend/model/model.go
+++ backend/model/model.go
@@ -17,7 +17,7 @@ type Response struct {
 type Sprint struct {
 	Id         string    `gorm:"primaryKey;not null;unique" json:"name" form:"name"  binding:"required" example:"ver2.55"`
 	StartDate  time.Time `json:"start_date" form:"start_date"  binding:"required" example:"2025-03-09T00:00:00Z"`
-	EndDate    time.Time `json:"end_date" form:"end_date" binding:"required" example:"2025-03-21T00:00:00Z"`
+	EndDate    time.Time `json:"end_date" form:"end_date" example:"2025-03-21T00:00:00Z"`
 	Plan       time.Time `json:"plan_date" form:"plan_date" example:"2025-03-21T00:00:00Z"`
 	Test       time.Time `json:"test_date" form:"test_date" example:"2025-03-21T00:00:00Z"`
 	Retro      time.Time `json:"retro_date" form:"retro_date" example:"2025-03-21T00:00:00Z"`
